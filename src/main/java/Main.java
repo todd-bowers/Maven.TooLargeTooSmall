a58176c4c873e41d7dc90220373e9dd017001489
todd-bowers <toddbows@gmail.com>
2022-06-28 22:30:24 -0400

    Gottem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+        int answer = 37;
+        boolean correctGuess;
+        System.out.println("Guess the number!");
+        int userInput = input.nextInt();
+
+        do {
+            if (userInput < answer) {
+                System.out.println("TOO LOW");
+                System.out.println("Guess again: ");
+                userInput = input.nextInt();
+                correctGuess = true;
+            } else if (userInput > answer) {
+                System.out.println("TOO HIGH");
+                System.out.println("Guess again: ");
+                userInput = input.nextInt();
+                correctGuess = true;
+            } else {
+                System.out.println("Correct answer!");
+                correctGuess = false;
+            }
+        } while(correctGuess);
     }
 }
